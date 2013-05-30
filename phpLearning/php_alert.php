adf0ece38457c163318bd9194b2d2596e37d92f9
CleanSky <zoubingsong@163.com>
2013-05-30 11:39:24 +0800

    php alert

---
+++ phpLearning/php_alert.php
@@ -0,0 +1,12 @@
+﻿<?php
+//********弹出alert框并跳转到指定页面******//
+function alert($message,$url='',$isAlert=true,$title='提示'){
+	echo '<html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><title>',$title,'</title></head><body>';
+	echo '<script type="text/javascript">';
+	echo $isAlert?'alert("'.$message.'");':'';
+	echo $url==''?'history.back();':'location.href="'.$url.'";';
+	echo '</script>';
+	echo '</body></html>';
+	exit();
+}
+?>
